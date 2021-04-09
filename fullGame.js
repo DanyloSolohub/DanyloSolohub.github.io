bf4428b6394d20b6c90ad9b9989a196c83a3c923
DanyloSolohub <73057792+DanyloSolohub@users.noreply.github.com>
2021-04-09 15:36:47 +0300

    Update fullGame.js

--- fullGame.js
+++ fullGame.js
@@ -274,7 +274,7 @@ function TheGame() {
         const diff = timestamp - pTime
         pTime = timestamp
         // fps кількість кадрів в секунду
-        const fps = Math.floor(1000 / diff) 
+        const fps = Math.round(1000 / diff) 
         // secondPart обернена величина до к-сті кадрів ,
         // потрібна для правильного оновлення
         const secondPart = diff / 1000
